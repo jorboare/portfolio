5042e944cd9049ab5c7cf7e3bb4b81c6b52a5df4
jorboare <jordi.b.arevalo@gmail.com>
2021-04-20 17:40:06 +0200

    contact modified

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -9,7 +9,6 @@ const Technologies = () => {
 
             <h2>Contacto</h2>
             <p>jordi.b.arevalo@gmail.com</p>
-            <p>+34 678 800 332</p>
             <div className='social-icons'>
                 <a href='https://www.linkedin.com/in/jordi-boronat-arevalo/' target='_blank' rel="noreferrer"><img src={linkedin} alt='linkedin' /></a>
                 <a href='https://github.com/jorboare' target='_blank' rel="noreferrer"><img src={github} alt='github' /></a>
